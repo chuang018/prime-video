ea121309b7f7b577c8af92dab568eda9caa922e1
unknown <calvinhuang13@gmail.com>
2022-10-10 12:18:59 +0700

    home page 50%

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import SwiperCore, {Autoplay, Keyboard, Pagination, Scrollbar, Zoom} from 'swiper';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class Tab1Page {
 
   categories:any = null;
+  slideOpt ={
+    initialSlide: 1,
+    speed: 200,
+  };
 
   constructor() {
     this.categories = "all";
+  }
+
+  slidesDidLoad(slides: IonSlides): void{
+    slides.startAutoplay();
   }
 
 }
